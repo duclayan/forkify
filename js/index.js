ce1e28565fce2734ad902e48b5e617b3cdcca340
duclayan <duclayandominique@gmail.com>
2020-10-18 18:28:33 +0400

    forkify 2.0

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 var searchItem = document.querySelector('.search__field')
 var searchForm = document.querySelector('.search')
 var recipeColumn = document.querySelector('.results__list')
+var displayColumn = document.querySelector('.recipe')
 var addToCart = document.querySelector('.btn-small.recipe__btn')
 console.log(addToCart)
 
@@ -17,8 +18,10 @@ const getRecipe = async function(id) {
     let result = await fetch(`https://forkify-api.herokuapp.com/api/get?rId=${id}`)
     return result.json()
 }
+// ====================================================================================================================================
+//                                                        SEARCH CLICK
+// ====================================================================================================================================
 
-// SEARCH CLICK
 searchForm.onsubmit = async function (e) {
         recipeColumn.innerHTML = ''
         console.log('Search Submit')
@@ -26,51 +29,203 @@ searchForm.onsubmit = async function (e) {
         e.preventDefault()
         var recipeList= await searchRecipe(searchItem.value)
 
-        // var recipe = new recipeList(recipeList)
-
         recipeList.recipes.forEach((item)=> {
-            appendRecipeList(item)
+            appendResultsList(item)
             console.log(item)
         })
 }
+// ====================================================================================================================================
+//                                                        RECIPE CLICK
+// ====================================================================================================================================
 
-// RECIPE CLICK
 document.querySelector('.results__list').onclick = async function(e) {
-    e.preventDefault()
 
+    e.preventDefault()
+// ====================================================================================================================================
+//                                                 CLASSES TO ORGANIZE THE CONTENTS
+// ====================================================================================================================================
+
+    // Access Recipe List
     let n = (e.path[2].className === 'results__link') ? 2:0;
 
     e.path[n].classList.add('results__link--active')
-    let code = e.path[n].href.split("#")
-    
-    let currentRecipe = await getRecipe(code[1])
-    appendRecipeInstructions(currentRecipe)
+    let recipeID = e.path[n].href.split("#")
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+    let currentRecipe = await getRecipe(recipeID[1])
+    let title = currentRecipe.recipe
+    let ingredients = currentRecipe.recipe.ingredients
 
-    // ADD TO CART
-    document.querySelector('.btn-small.recipe__btn').onclick = async function(x){
+    // Create SUPER Class/ General Class 
+    currentRecipe = new recipeDisplay(title,ingredients)
+    
+    // Creates ingredientItem Class
+    let ingredientItem = []
+    currentRecipe.ingredientList.forEach((e,index) => {
+        ingredientItem[index] = new ingredient(e)
+    })
+    console.log(currentRecipe)
+    console.log(ingredientItem)
+// ====================================================================================================================================
+//                                        DISPLAY THE CONTENT IN THE MIDDLE UPON CLICK 
+// ====================================================================================================================================
 
-        var recipeItems = document.getElementsByClassName('recipe__item')
-        console.log(recipeItems)
+    appendRecipeHeader(currentRecipe.content)
+    appendRecipeDetails(currentRecipe)
+
+    // // ADD TO CART
+    // document.querySelector('.btn-small.recipe__btn').onclick = async function(x){
+
+    //     var recipeItems = document.getElementsByClassName('recipe__item')
+    //     console.log(recipeItems)
     
-        for (i = 0; i < recipeItems.length; i++) {
-            console.log(recipeItems[i])
-           let count = recipeItems[i].querySelector('.recipe__count').innerHTML
-           let unit = recipeItems[i].querySelector('.recipe__unit').innerHTML
-           let ingredient = recipeItems[i].querySelector('.recipe__ingredient').innertext
+    //     for (i = 0; i < recipeItems.length; i++) {
+    //     //     console.log(recipeItems[i])
+    //     //    let count = recipeItems[i].querySelector('.recipe__count').innerHTML
+    //     //    let unit = recipeItems[i].querySelector('.recipe__unit').innerHTML
+    //     //    let ingredient = recipeItems[i].querySelector('.recipe__ingredient').innertext
     
-           console.log(`Count: ${count}, Unit: ${unit}, Ingredient: ${ingredient}`)
-        }
+    //     //    console.log(`Count: ${count}, Unit: ${unit}, Ingredient: ${ingredient}`)
+
+    //     console.log(isMeasured(recipeItems[i]))
+    //     }
     
-    }
+    // }
 }
+
 // CLASS
-class recipeList {
-    constructor(recipeItems){
-        this.recipeItems = recipeItems
+
+class recipeDisplay {
+    
+    constructor(currentRecipe,ingredients) {
+        // this.content = contains object currentRecipe
+        // this.ingredientList = contains array of ingredients
+        this.content = currentRecipe
+        this.ingredientList = ingredients
+    
+    }
+    isMeasured(text){
+        // Checks whether the item is to be measured
+        // Split the content 
+        // let ingredientContent = e.split(' ')
+
+        let status = (isNaN(parseFloat(text[0].charAt(0)))) ? false: true;
+        return status
+    }
+
+    getUnit(text) {
+        // This function gets the unit by: Splitting the sentence into words then checks for the first string then returns it.
+
+        for (let i = 0 ; i < text.length ; i++) {
+            // Once a text is seen, it returns the value
+            if(isNaN(parseFloat(text[i]))){
+                return text[i]
+            }
+        }
+    }
+
+    isDashed(item){
+        // CHECK FOR PRESENCE OF DASH
+        // 1. Split the word per character
+        var content = item.split('')
+
+        // 2. IF '-' found; returns TRUE
+        for( let i = 0; i < content.length ; i++){
+            if (content[i] == '-') { return true } 
+        }
+        return false
+    }
+
+    getValue(text) {   
+        // Check whether the ingredient has dash or not
+        // Only takes in the first word
+        if (this.isDashed(text[0]) === true && this.isMeasured === true) {
+            // WITH DASH
+            // If it contains a dash- it splits the text and stores in an array
+            // Return added value of the stored value
+            text = text[0].split('-')
+            return (eval(text[0]) + eval(text[1])).toFixed(1)
+        } else {
+            // WITHOUT DASH 
+            // Checks whether there is only one value to be added
+            if (isNaN(text[1])){
+                return eval(text[0]).toFixed(1)
+            } else {
+                return (eval(text[0]) + eval(text[1])).toFixed(1)
+            }
+        }
+
+        
+    }
+
+    getItemDescription(content){
+        // this function removes the parenthesis of the string 
+        let start = content.split("(")[0]
+        let end = content.split(")")[1]
+
+        return (end != undefined) ? `${start} ${end}`: start
+    }
+
+    getItem(content,unit){
+        // call get item description to remove all the close parenthesis
+        // for items with no unit, no need to split
+        // for items with unit, take the second part of the splitted string
+        return (unit === undefined)? this.getItemDescription(content):this.getItemDescription(content).split(unit)[1]
+    }
+
+    calcTime() {
+        // Assuming that we need 15 min for each 3 ingredients
+        console.log ('calc time has been called')
+        const numIng = this.content.ingredients.length;
+        const periods = Math.ceil(numIng / 3);
+        this.time = periods * 15;
+        console.log(this.content)
+
+    }
+    
+  }
+
+  class ingredient extends recipeDisplay {
+
+    constructor(content,currentRecipe,ingredients){
+        // =============================================================================================
+        // Constructor Description
+        // =============================================================================================
+        // this.content = contains the whole object
+        // this.description = Ingredient Description in full form (includes measurement and item)
+        // this.isMeasured = Checks whether the product contains measurements (e.g: cups, tbsp,tsp)
+        // this.unit = checks for unit (e.g: cups, tbsp,tsp)
+        // this.value = takes in the total value in decimals('1.5, 1.33'); rounded to one decimal place
+        // this.item = holds produc description
+        // =============================================================================================
+
+        super(currentRecipe,ingredients)
+
+        this.content = currentRecipe
+        this.description = content
+        this.textContent = this.getItemDescription(content).split(' ')
+        this.isMeasured = this.isMeasured(this.textContent)
+        // Based on whether it's measured or not, the program will take the ingredient's unit and value 
+
+        if(this.isMeasured == true){ 
+            this.unit = this.getUnit(this.textContent)
+            this.value = this.getValue(this.textContent)
+        } else {
+            this.value = 1
+        } 
+
+        this.item = this.getItem(this.description,this.unit)
+
     }
 }
+
+
+
 //FUNCTIONS
-const appendRecipeList= async function (item) {
+// ==================================================================================================================================================================
+
+// ==================================================================================================================================================================
+
+const appendResultsList= async function (item) {
     let newHTML = 
     `<li>
         <a class="results__link" href="#${item.recipe_id}">
@@ -85,156 +240,54 @@ const appendRecipeList= async function (item) {
     </li>`
     recipeColumn.insertAdjacentHTML('beforeend',newHTML)
 }
-const appendRecipeInstructions = async function(item) {
-    let newRecipe = 
-    ` <figure class="recipe__fig">
-        <img src="${item.recipe.image_url}" class="recipe__img">
-        <h1 class="recipe__title">
-            <span>Pasta with tomato cream sauce</span>
-        </h1>
+const appendRecipeHeader = async function (item){
+    let headerHTML = 
+    `
+    <figure class="recipe__fig">
+                <img src="${item.image_url}" alt="Tomato" class="recipe__img">
+                <h1 class="recipe__title">
+                    <span>${item.title}</span>
+                </h1>
     </figure>
-
+    `
+    displayColumn.insertAdjacentHTML('beforeend',headerHTML)
+}
+const appendRecipeDetails = async function (item) {
+    item.calcTime()
+    let recipe_infoHTML = 
+    `
     <div class="recipe__details">
-
-        <div class="recipe__info">
-            <svg class="recipe__info-icon">
-                <use href="img/icons.svg#icon-stopwatch"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">45</span>
-            <span class="recipe__info-text"> minutes</span>
-        </div>
-        <div class="recipe__info">
-            <svg class="recipe__info-icon">
-                <use href="img/icons.svg#icon-man"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--people">4</span>
-            <span class="recipe__info-text"> servings</span>
-
-            <div class="recipe__info-buttons">
-                <button class="btn-tiny">
-                    <svg>
-                        <use href="img/icons.svg#icon-circle-with-minus"></use>
-                    </svg>
-                </button>
-                <button class="btn-tiny">
-                    <svg>
-                        <use href="img/icons.svg#icon-circle-with-plus"></use>
-                    </svg>
-                </button>
-            </div>
-
-        </div>
-        <button class="recipe__love">
-            <svg class="header__likes">
-                <use href="img/icons.svg#icon-heart-outlined"></use>
-            </svg>
-        </button>
+    <div class="recipe__info">
+        <svg class="recipe__info-icon">
+            <use href="img/icons.svg#icon-stopwatch"></use>
+        </svg>
+        <span class="recipe__info-data recipe__info-data--minutes">${item.time}</span>
+        <span class="recipe__info-text"> minutes</span>
     </div>
-    
-    <div class="recipe__ingredients">
-        <ul class="recipe__ingredient-list">
-        </ul>
+    <div class="recipe__info">
+        <svg class="recipe__info-icon">
+            <use href="img/icons.svg#icon-man"></use>
+        </svg>
+        <span class="recipe__info-data recipe__info-data--people">4</span>
+        <span class="recipe__info-text"> servings</span>
 
-        <button class="btn-small recipe__btn">
-            <svg class="search__icon">
-                <use href="img/icons.svg#icon-shopping-cart"></use>
-            </svg>
-            <span>Add to shopping list</span>
-        </button>
+        <div class="recipe__info-buttons">
+            <button class="btn-tiny">
+                <svg>
+                    <use href="img/icons.svg#icon-circle-with-minus"></use>
+                </svg>
+            </button>
+            <button class="btn-tiny">
+                <svg>
+                    <use href="img/icons.svg#icon-circle-with-plus"></use>
+                </svg>
+            </button>
+        </div>
+
     </div>
-
-    <div class="recipe__directions">
-        <h2 class="heading-2">How to cook it</h2>
-        <p class="recipe__directions-text">
-            This recipe was carefully designed and tested by
-            <span class="recipe__by">The Pioneer Woman</span>. Please check out directions at their website.
-        </p>
-        <a class="btn-small recipe__btn" href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/" target="_blank">
-            <span>Directions</span>
-            <svg class="search__icon">
-                <use href="img/icons.svg#icon-triangle-right"></use>
-            </svg>
-
-        </a>`
-    document.querySelector('.recipe').insertAdjacentHTML('beforeend',newRecipe)
-
-
-    item.recipe.ingredients.forEach((ingredient) => {
-        let ingredients = ingredient.split(" ")
-        var ingredientName = ''
-        var addIngredient 
-        var ingredientCount 
-        var ingredientUnit 
-        var i 
-
-        if (parseInt(ingredient[0]) != NaN) {
-            i = 2
-            ingredientCount = ingredients[0] 
-            ingredientUnit = ingredients[1]
-        } else {
-            i = 0
-            ingredientCount = ''
-            ingredientUnit = ''
-        }
-    
-        for (i ; i < ingredients.length ; i++){
-            ingredientName += ` ${ingredients[i]}`
-        }
-    
-        if (parseInt(ingredients[0]) != NaN) {
-            addIngredient = 
-            `<li class="recipe__item">
-                <svg class="recipe__icon">
-                    <use href="img/icons.svg#icon-check"></use>
-                </svg>
-                <div class="recipe__count">${ingredientCount}</div>
-                <div class="recipe__ingredient">
-                    <span class="recipe__unit">${ingredientUnit}</span>
-                    ${ingredientName}
-                </div>
-            </li>`
-            
-        } else { 
-            addIngredient = 
-            `<li class="recipe__item">
-                <svg class="recipe__icon">
-                    <use href="img/icons.svg#icon-check"></use>
-                </svg>
-                <div class="recipe__ingredient">
-                    ${ingredientName}
-                </div>
-            </li>`
-        }
-        console.log(ingredient)
-    
-    
-        if (i = 1)
-        document.querySelector('.recipe__ingredient-list').insertAdjacentHTML('beforeend',addIngredient)
-    }
-    )
-}
-
-
-
-const appendShoppingList =async function(item){
-
-    console.log('appendShopping')
-
-    let shoppingItem = 
     `
-    <li class="shopping__item">
-                <div class="shopping__count">
-                    <input type="number" value="500" step="100">
-                    <p>g</p>
-                </div>
-
-                <p class="shopping__description">Pasta</p>
-
-                <button class="shopping__delete btn-tiny">
-                    <svg>
-                        <use href="img/icons.svg#icon-circle-with-cross"></use>
-                    </svg>
-                </button>
-     </li>
-    `
+    displayColumn.insertAdjacentHTML('beforeend',recipe_infoHTML)
 }
+// ==================================================================================================================================================================
+
+// ==================================================================================================================================================================
